37997a1ada448b51eddd7eedb05cdf05b7c2320a
Vanessa <bremerich@igb-berlin.de>
2017-02-08 09:39:02 +0100

    new geonode project for FIP geonode created from template using local virtuelenv

---
+++ setup.py
@@ -0,0 +1,28 @@
+import os
+from distutils.core import setup
+
+def read(*rnames):
+    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
+
+setup(
+    name="fip_geonode",
+    version="0.1",
+    author="",
+    author_email="",
+    description="fip_geonode, based on GeoNode",
+    long_description=(read('README.rst')),
+    # Full list of classifiers can be found at:
+    # http://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        'Development Status :: 1 - Planning',
+    ],
+    license="BSD",
+    keywords="fip_geonode geonode django",
+    url='https://github.com/fip_geonode/fip_geonode',
+    packages=['fip_geonode',],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+       'geonode>=2.5',
+    ],
+)
